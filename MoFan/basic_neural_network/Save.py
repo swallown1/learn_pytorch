3a41ff537c13d13b58ee77ed5229b6e8419e9e7a
swallown1 <2327257266@qq.com>
2020-08-04 13:57:47 +0800

    save model

--- MoFan/basic_neural_network/Save.py
+++ MoFan/basic_neural_network/Save.py
@@ -1,28 +1,41 @@
 import torch
+import matplotlib.pyplot as plt
 
 x = torch.unsqueeze(torch.linspace(-1,1,100),dim=1)
 
 y = x.pow(2) + 0.2*torch.rand(x.size())
 
-net = torch.nn.Sequential(
-    torch.nn.Linear(1,10),
-    torch.nn.ReLU(),
-    torch.nn.Linear(10,1)
-)
+def save():
+    # save net1
+    net1 = torch.nn.Sequential(
+        torch.nn.Linear(1, 10),
+        torch.nn.ReLU(),
+        torch.nn.Linear(10, 1)
+    )
+    optimizer = torch.optim.SGD(net1.parameters(), lr=0.5)
+    loss_func = torch.nn.MSELoss()
 
-optimizer = torch.optim.SGD(net.parameters(),lr=0.01)
-loss_fun = torch.nn.MSELoss()
+    for t in range(100):
+        prediction = net1(x)
+        loss = loss_func(prediction, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-for i in range(100):
-    pred = net(x)
-    loss= loss_fun(pred,y)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    print(str(i)+": loss "+str(loss))
+    # plot result
+    plt.figure(1, figsize=(10, 3))
+    plt.subplot(131)
+    plt.title('Net1')
+    plt.scatter(x.data.numpy(), y.data.numpy())
+    plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
 
-# torch.save(net,'net.pkl')
-torch.save(net.state_dict(),'net_param.pkl')
+    # 2 ways to save the net
+    torch.save(net1, 'net.pkl')  # save entire net
+    torch.save(net1.state_dict(), 'net_param.pkl')   # save only the parameters
+
+
+
+
 
 
 def restore_net(x):
@@ -31,3 +44,29 @@ def restore_net(x):
     return pred
 
 # print(restore_net(x),y)
+
+def restore_params():
+    # 新建 net3
+    net3 = torch.nn.Sequential(
+        torch.nn.Linear(1, 10),
+        torch.nn.ReLU(),
+        torch.nn.Linear(10, 1)
+    )
+
+    # 将保存的参数复制到 net3
+    net3.load_state_dict(torch.load('net_param.pkl'))
+    prediction = net3(x)
+
+    # plot result
+    plt.subplot(133)
+    plt.title('Net3')
+    plt.scatter(x.data.numpy(), y.data.numpy())
+    plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+    plt.show()
+
+
+save()
+
+
+restore_params()
+
